b00d551dcef0aa16084b6284918aed0ecf9caf53
HyunSang8421 <128012383+HyunSang8421@users.noreply.github.com>
2023-12-12 10:33:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
+def hello():
+	return "Hello World!"
+
+@app.route('/hello')
 def hello():
 	return "Hello World!"
 
